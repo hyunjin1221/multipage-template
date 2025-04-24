35995848a31a3dec60936eb3fab54d530068bd9d
hyunjin1221 <111889188+hyunjin1221@users.noreply.github.com>
2025-04-24 19:22:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # **** Page layout setup ****
 App_page_0 = st.Page(
-    "pages/streamlit (1).py",
+    "pages/main.py",
     title="Click here to select stock",
     default=True
 )
