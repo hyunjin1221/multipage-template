a911c842fbaeec193c8e0969d4009a93a4732939
hyunjin1221 <111889188+hyunjin1221@users.noreply.github.com>
2025-04-26 17:33:35 -0400

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -1,11 +1,28 @@
 import streamlit as st
+import pandas as pd
+import yfinance as yf
 
-# Introduction Story
-st.markdown("""
-**Investing is both an art and a science — and we're here to make it smarter and simpler for you.**
+# Print out info for the Main page:
+st.title("Main Page: Please select a stock")
+st.header("<---- Enter ticker & dates in Sidebar Prompts.")
 
-In today’s fast-moving markets, successful investing requires more than just watching the headlines.  
-It demands a balanced view of a company's **long-term financial health** and its **short-term market momentum**.
+# Sidebar inputs for ticker symbol and dates
+ticker_symbol = st.sidebar.text_input("Enter Stock Ticker (e.g., AAPL, MSFT)", value="MSFT")
+start_date = st.sidebar.date_input("Start Date", value=pd.to_datetime("2024-01-01"))
+end_date = st.sidebar.date_input("End Date", value=pd.to_datetime("2024-12-31"))
 
-That's why we built the **Stock Advisory Tool** — a data-driven platform that helps you make more informed investment decisions with just a few clicks.
-""")
+# Print update onb which ticker and dates
+st.write(f"You have selected **{ticker_symbol}** from {start_date} to {end_date}.")
+st.markdown(''':red[Now click on] :blue-background[Page 1, 2 or 3] to the left to view analyses.''')
+
+# Access the stock data for the given tocker using the yfinance "download" function
+# Temporarily store data in "df" dataframe
+df = yf.download(ticker_symbol, start=start_date, end=end_date)
+if df.empty:
+   st.error("No data found. Please check the ticker symbol or date range.")
+   st.stop()
+
+
+#  Note: The stock info dataframe (df) is stored in a StreamLit "session state" that allows the data to be shared across
+#        the mutiple pages (ie, main.py, page1.py, page2.py and page4.py)
+st.session_state.data = df
